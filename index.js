1eec383f335f1488a3806309afec12705cadd702
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2022-10-31 14:49:47 +0100

    Creation of all models Cart, Order, Product, User

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
 
+app.use(express.json());
+app.use("/api/users", userRoute);
+
+dotenv.config();
 mongoose
-  .connect(
-    "mongodb+srv://jorgedasilva:Melun1972@cluster0.nhde3.mongodb.net/shop?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGO_URL)
   .then(() => console.log("DB Connection Successfull!"))
   .catch((err) => {
     console.log(err);
   });
+
+app.get("/api/test", () => {
+  console.log("test is Successfull!");
+});
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
